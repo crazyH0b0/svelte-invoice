94569c325cf5c464719bfde3b7a8c9882cf74873
6666 <1327185450@qq.com>
2023-08-28 00:05:53 +0800

    add navbar

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,37 @@
+<script>
+  import { page } from '$app/stores';
+</script>
+
+<header class=" col-span-3 bg-daisyBush text-center">
+  <div>
+    <a href="/invoices">
+      <img src="/images/logo.svg" alt="The Dollar Holler " class=" mx-auto" />
+    </a>
+    <nav>
+      <ul class=" list-none text-2xl font-bold">
+        <li><a href="/invoices" class:active={$page.url.pathname === '/invoices'}>Invoices</a></li>
+        <li><a href="/clients" class:active={$page.url.pathname === '/clients'}>Clients</a></li>
+        <li><a href="#">Setting</a></li>
+        <li><a href="#">logout</a></li>
+      </ul>
+    </nav>
+  </div>
+</header>
+
+<style lang="postcss">
+  nav ul li {
+    @apply mb-6;
+  }
+  nav ul li a {
+    @apply text-white hover:text-goldenFizz px-8;
+  }
+
+  nav ul li a.active {
+    @apply text-robinEggBlue transition-[padding];
+    background: url('/images/active-nav--left.svg') no-repeat left top,
+      url('/images/active-nav--right.svg') no-repeat right top;
+  }
+  nav ul li a.active:hover {
+    @apply px-9;
+  }
+</style>
--- src/routes/(dashboard)/+layout.svelte
+++ src/routes/(dashboard)/+layout.svelte
@@ -1,30 +1,10 @@
-<div class=" grid grid-cols-12 gap-x-16 bg-whisper min-h-screen">
-  <header class=" col-span-3 bg-daisyBush text-center">
-    <div>
-      <a href="/invoices">
-        <img src="/images/logo.svg" alt="The Dollar Holler " class=" mx-auto" />
-      </a>
-      <nav>
-        <ul class=" list-none text-2xl font-bold">
-          <li><a href="/invoices">Invoices</a></li>
-          <li><a href="/clients">Clients</a></li>
-          <li><a href="#">Setting</a></li>
-          <li><a href="#">logout</a></li>
-        </ul>
-      </nav>
-    </div>
-  </header>
+<script lang="ts">
+  import Navbar from '$lib/components/Navbar.svelte';
+</script>
 
+<div class=" grid grid-cols-12 gap-x-16 bg-whisper min-h-screen">
+  <Navbar />
   <main>
     <slot />
   </main>
 </div>
-
-<style lang="postcss">
-  nav ul li {
-    @apply mb-6;
-  }
-  nav ul li a {
-    @apply text-white hover:text-goldenFizz;
-  }
-</style>
