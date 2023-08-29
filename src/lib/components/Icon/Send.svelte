da2242ece8d3fc7e3daba24de8e33eae0f4cc024
6666 <1327185450@qq.com>
2023-08-29 14:11:32 +0800

    add svg

--- src/lib/components/Icon/Send.svelte
+++ src/lib/components/Icon/Send.svelte
@@ -4,7 +4,5 @@
 </script>
 
 <svg {width} {height} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path d="M24 6.75007H0V5.25005H24V6.75007Z" />
-  <path d="M24 12.7501H0V11.2501H24V12.7501Z" />
-  <path d="M24 18.7502H0V17.2502H24V18.7502Z" />
+  <path d="M2.01 21.0002L23 12.0002L2.01 3.00024L2 10.0002L17 12.0002L2 14.0002L2.01 21.0002Z" />
 </svg>
